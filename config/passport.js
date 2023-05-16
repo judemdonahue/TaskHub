19e26f84f8b739795d9d8d47d756a553b1b216e3
Landon Heaney <landonheaney@gmail.com>
2023-05-15 20:45:39 -0500

    login middleware + passport

---
+++ config/passport.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+},
+  async (email, password, done) => {
+    // Tries to find the user in our database
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+        console.log('wrong email')
+      return done(null, false, { message: 'wrong email.' });
+    }
+    // Checks if the password is correct
+    if (!user.checkPassword(password)) {
+        console.log('wrong password')
+      return done(null, false, { message: 'wrong password.' });
+    }
+    return done(null, user);
+  }
+));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
+  });
+  
+  module.exports = passport;
