77038a0a69f54676ad7540421bd2afb8a54f69a7
Rosa Alonso <alonso_2018@yahoo.com>
2023-05-16 14:00:00 -0500

    worked on getting total points for the user leaderboard

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const session = require('express-session');
-const passport = require('./config/passport');
-const sequelize = require('./config/connection');
+const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
+const sequelize = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,14 +10,20 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Sessions
-app.use(session({ secret: 'eventually-in-env', resave: true, saveUninitialized: true }));
+app.use(
+  session({
+    secret: "eventually-in-env",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Routes These can be cleaned up a bit Tutor Question? 
+// Routes These can be cleaned up a bit Tutor Question?
 app.use("/", require("./layouts/main.handlebars"));
 
 app.use("/homepage", require("./views/homepage.handlebars"));
@@ -26,5 +33,5 @@ app.use("/user", require("./views/user.handlebars"));
 app.use("/login", require("./views/login.handlebars"));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
-  });
+  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+});
