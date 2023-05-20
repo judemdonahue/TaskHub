0ac336288b4184a432a65a2ebcc86f0e5177c5db
Rosa Alonso <alonso_2018@yahoo.com>
2023-05-20 11:27:09 -0500

    added the post route in the tasks.js that switches task to completed

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,3 +12,26 @@ router.get("/tasks/:id", async (req, res) => {
   res.render("homepage", { tasks });
 });
 // pulls id and attributes from task model
+
+// GET method route for task to completed
+app.get("/UserTask/:id", (req, res) => {
+  res.render("user-card");
+});
+
+// UPDATE a user
+router.put("../models/UserTask/:id", async (req, res) => {
+  try {
+    const userData = await User.update(req.body, {
+      where: {
+        completed: req.params.completed,
+      },
+    });
+    if (!userData[0]) {
+      res.status(404).json({ message: "No user has completed" });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
