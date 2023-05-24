b3c844d40078142f40fe9f6e68d03b299f4927d6
AntiqueRised <imjoodie@gmail.com>
2023-05-23 21:35:28 -0500

    config

--- config/connection.js
+++ config/connection.js
@@ -16,15 +16,5 @@ if (process.env.JAWSDB_URL) {
   );
 }
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: "localhost",
-//     dialect: "mysql",
-//     port: 3306,
-//   }
-// );
 
 module.exports = sequelize;
